211ba8fcf881290597ee4cf7dac22a66bedfedec
Tran Nhat Huy <nhathuy1237890@gmail.com>
2025-04-26 10:19:47 +0700

    test deloy

--- src/server.js
+++ src/server.js
@@ -62,13 +62,16 @@ app.use('/', telegramWebhook);
 //     .catch(err => {
 //         console.error('Kết nối MongoDB thất bại', err);
 //     });
-mongoose.connect('mongodb://mongo:couQibWXAIZRTfDWQjjCSajfOiPZpnhR@caboose.proxy.rlwy.net:51891/otobathanh', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Kết nối MongoDB thành công');
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Kết nối MongoDB thất bại', err);
+mongoose.connect('mongodb://mongo:couQibWXAIZRTfDWQjjCSajfOiPZpnhR@caboose.proxy.rlwy.net:51891', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => {
+    console.log('Kết nối MongoDB thành công');
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
     });
+})
+.catch(err => {
+    console.error('Kết nối MongoDB thất bại', err);
+});
